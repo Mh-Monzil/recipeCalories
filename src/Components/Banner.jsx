848bb1910e3d318f53997b47e7d46d9e68246635
Mh-Monzil <monzil246@gmail.com>
2024-03-16 21:29:28 +0600

    leading added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     }
     return (
         <div style={bg} className="w-full max-h-[600px] px-6 py-10 md:py-32 md:px-52 text-white text-center bg-cover bg-center rounded-[24px]">
-            <h1  className="text-3xl md:text-[52px] font-bold md:leading-[72px]">Discover Your Custom Cooking Class Adventure!</h1>
+            <h1  className="text-3xl md:text-[52px] font-bold leading-9 md:leading-[72px]">Discover Your Custom Cooking Class Adventure!</h1>
             <p className="font-medium text-lg pt-3 md:pt-6">Explore custom-tailored cooking classes designed to elevate your culinary skills and ignite your passion for food. Your gourmet journey starts here!</p>
             <div className="flex justify-center gap-6 items-center pt-10">
                 <button className="px-5 py-3 md:px-7 md:py-5 bg-green-400 rounded-[50px] md:text-xl font-semibold border-2 border-green-400 text-black hover:bg-transparent hover:border-white    hover:text-white ease-in-out transition-all">Explore Now</button>
